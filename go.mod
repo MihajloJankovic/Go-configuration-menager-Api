c88cb34bc1af181248a134078d4af4b0f087f79b
MihajloJankovic <101173449+MihajloJankovic@users.noreply.github.com>
2023-05-02 08:42:18 +0200

    Server

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/MihajloJankovic/Alati/tree/develop
 
 go 1.19
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/mux v1.8.0
+)
---
+++ server.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"errors"
+	"github.com/gorilla/mux"
+	"mime"
+	"net/http"
+)
+
+type postServer struct {
+	data map[string]*Config
+}
+
+func (ts *postServer) createPostHandler(w http.ResponseWriter, req *http.Request) {
+	contentType := req.Header.Get("Content-Type")
+	mediatype, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if mediatype != "application/json" {
+		err := errors.New("Expect application/json Content-Type")
+		http.Error(w, err.Error(), http.StatusUnsupportedMediaType)
+		return
+	}
+
+	rt, err := decodeBody(req.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := createId()
+	rt.Id = id
+	ts.data[id] = rt
+	renderJSON(w, rt)
+}
+
+func (ts *postServer) getAllHandler(w http.ResponseWriter, req *http.Request) {
+	allCons := []*Config{}
+	for _, v := range ts.data {
+		allCons = append(allCons, v)
+	}
+
+	renderJSON(w, allCons)
+}
+
+func (ts *postServer) getPostHandler(w http.ResponseWriter, req *http.Request) {
+	id := mux.Vars(req)["id"]
+	config, ok := ts.data[id]
+	if !ok {
+		err := errors.New("key not found")
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	renderJSON(w, config)
+}
+
+func (ts *postServer) delPostHandler(w http.ResponseWriter, req *http.Request) {
+	id := mux.Vars(req)["id"]
+	if v, ok := ts.data[id]; ok {
+		delete(ts.data, id)
+		renderJSON(w, v)
+	} else {
+		err := errors.New("key not found")
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
+}
