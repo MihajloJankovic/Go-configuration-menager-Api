f1167d4cf14898e9cfaa3d3aa1e76deb59ce92e0
Sovlja <101639981+Sovlja@users.noreply.github.com>
2023-05-02 20:46:53 +0200

    server.go updated

--- server.go
+++ server.go
@@ -10,7 +10,8 @@ import (
 )
 
 type postServer struct {
-	data map[string]*Config
+	dataConfig map[string]*Config
+	data map[string]*ConfigGroup
 }
 
 
@@ -36,14 +37,14 @@ func (ts *postServer) createPostHandler(w http.ResponseWriter, req *http.Request
 
 	id := createId()
 	rt.Id = id
-	ts.data[id] = rt
+	ts.dataConfig[id] = rt
 	renderJSON(w, rt)
 }
 
 
 func (ts *postServer) getAllHandler(w http.ResponseWriter, req *http.Request) {
 	allCons := []*Config{}
-	for _, v := range ts.data {
+	for _, v := range ts.dataConfig {
 		allCons = append(allCons, v)
 	}
 
@@ -52,7 +53,7 @@ func (ts *postServer) getAllHandler(w http.ResponseWriter, req *http.Request) {
 
 func (ts *postServer) getPostHandler(w http.ResponseWriter, req *http.Request) {
 	id := mux.Vars(req)["id"]
-	config, ok := ts.data[id]
+	config, ok := ts.dataConfig[id]
 	if !ok {
 		err := errors.New("key not found")
 		http.Error(w, err.Error(), http.StatusNotFound)
@@ -65,7 +66,81 @@ func (ts *postServer) delPostHandler(w http.ResponseWriter, req *http.Request) {
 	id := mux.Vars(req)["id"]
 	log.Println(id)
 
-	delete(ts.data, id)
-	renderJSON(w, ts.data[0])
+	delete(ts.dataConfig, id)
+	renderJSON(w, ts.dataConfig[0])
 
+}
+
+
+
+func (ts *postServer) createConfigGroupHandler(w http.ResponseWriter, req *http.Request) {
+    contentType := req.Header.Get("Content-Type")
+    mediatype, _, err := mime.ParseMediaType(contentType)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    if mediatype != "application/json" {
+        err := errors.New("Expected application/json Content-type")
+        http.Error(w, err.Error(), http.StatusUnsupportedMediaType)
+        return
+    }
+
+
+    rt, err := decodeGroupBody(req.Body)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    id := createId()
+    rt.Id = id
+    ts.data[id] = rt
+    renderJSON(w, rt)
+
+    log.Println(rt.Id)
+}
+
+func (ts *postServer) getAllConfigGroupHandlers(w http.ResponseWriter, req *http.Request) {
+    allTasks := []*Group{}
+    for _, v := range ts.data {
+        allTasks = append(allTasks, v)
+    }
+    renderJSON(w, allTasks)
+}
+
+func (ts *postServer) getConfigGroupHandler(w http.ResponseWriter, req *http.Request) {
+    id := mux.Vars(req)["id"]
+    task, ok := ts.data[id]
+    if !ok {
+        err := errors.New("id not found")
+        http.Error(w, err.Error(), http.StatusNotFound)
+        return
+    }
+    renderJSON(w, task)
+}
+
+func (ts *postServer) delConfigGroupHandler(w http.ResponseWriter, req *http.Request) {
+    id := mux.Vars(req)["id"]
+    if v, ok := ts.data[id]; ok {
+        delete(ts.data, id)
+        renderJSON(w, v)
+    } else {
+        err := errors.New("id not found")
+        http.Error(w, err.Error(), http.StatusNotFound)
+    }
+}
+
+func (ts *postServer) addConfigInConfigGroup(w http.ResponseWriter, req *http.Request){
+	id := req.URL.Query().Get("id")
+	config := req.URL.Query().Get("config")
+	configGroup := req.URL.Query().Get("configGroup")
+
+	configW := ts.dataConfig[config]
+	configGroupW := ts.data[configGroup]
+
+	configGroupW.Configs[config] = configW
+
+	renderJSON(w, configGroupW)
 }
