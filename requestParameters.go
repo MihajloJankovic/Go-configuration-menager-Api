49149209ccb85977baaedfdf94452416ba724d7f
MihajloJankovic <101173449+MihajloJankovic@users.noreply.github.com>
2023-05-24 11:36:58 +0200

    Revert "Update requestParameters.go"
    This reverts commit d160fd53db087e1a2fb73fd33e78284456b3c5ab.

--- requestParameters.go
+++ requestParameters.go
@@ -1,6 +1,6 @@
 package main
 
-// swagger:parameters deleteConfig
+// swagger:parameters deletePost
 type DeleteRequest struct {
 	// Post ID
 	// in: path
@@ -14,37 +14,8 @@ type GetRequest struct {
 	Id string `json:"id"`
 }
 
-<<<<<<< Updated upstream
 // swagger:parameters post createPost
 type RequestPostBody struct {
-=======
- // swagger:parameters post createConfigGroup
-type RequestConfigGroupBody struct {
-	// - name: body
-	//  in: body
-	//  description: name and status
-	//  schema:
-	//  type: object
-	//     "$ref": "#/definitions/RequestPost"
-	//  required: true
-	Body RequestPost `json:"body"`
- }
-
-// swagger:parameters post createConfig
-type RequestConfigBody struct {
-	// - name: body
-	//  in: body
-	//  description: name and status
-	//  schema:
-	//  type: object
-	//     "$ref": "#/definitions/RequestPost"
-	//  required: true
-	Body RequestPost `json:"body"`
- }
-
- // swagger:parameters post getConfigs
-type RequestConfigBody struct {
->>>>>>> Stashed changes
 	// - name: body
 	//  in: body
 	//  description: name and status
